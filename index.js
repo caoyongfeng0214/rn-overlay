f79c5721bdcaa3d5d68d45a836840992af02d756
caoyongfeng0214 <caoyongfeng0214@gmail.com>
2020-11-21 04:38:20 +0800

    added scopeState

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { View, ScrollView, Modal, AppRegistry, I18nManager, StyleSheet, Platform } from 'react-native';
+import { View, ScrollView, Modal, BackHandler, Platform, AppRegistry, I18nManager, StyleSheet } from 'react-native';
 import RCTModalHostView from '../react-native/Libraries/Modal/RCTModalHostViewNativeComponent';
 const AppContainer = require('../react-native/Libraries/ReactNative/AppContainer');
 
@@ -27,8 +27,8 @@ if(!ReactNative.Overlay) {
         static INDEX = 0; // for generate OverlayName
 
         // show a Overlay
-        static show = function({ style, children, onShow, onClose }) {
-            let overlay = new ReactNative.Overlay({ style, children, onShow, onClose, __noOwner__: true });
+        static show = function({ style, scopeState, children, onShow, onClose }) {
+            let overlay = new ReactNative.Overlay({ style, scopeState, children, onShow, onClose, __noOwner__: true });
             overlay.componentDidMount();
             overlay.show();
             return overlay;
@@ -41,6 +41,8 @@ if(!ReactNative.Overlay) {
             let _name = '_overlay_' + (++ReactNative.Overlay.INDEX);
             Object.defineProperty(this, '$Name', { get: function() {return _name;} });
 
+            let _hardwareBackPressHandler = undefined;
+
             let _visible = false;
             Object.defineProperty(this, 'visible', {
                 get: function() { return _visible; },
@@ -48,6 +50,11 @@ if(!ReactNative.Overlay) {
                     let old = _visible;
                     _visible = val;
                     this.$Father.addOrUpdateOverlay(this);
+                    if(val) {
+                        this.disableBackPress();
+                    } else {
+                        this.removeDisableBackPress();
+                    }
                     if(this.props.onClose) {
                         if(old && !val) {
                             this.props.onClose.apply(this, []);
@@ -60,20 +67,37 @@ if(!ReactNative.Overlay) {
                     }
                 }
             });
-        }
-      
-        get $View() {
-            let sty = {backgroundColor: 'rgba(88, 88, 88, 0.6)', ...(this.props.style || {})};
-            // if not defined STYLE, or defined POSITON(left|right|top|bottom), then use default value({left:0, right:0, top:0, bottom:0})
-            if(!this.props.style
-                || (this.props.style.left === undefined && this.props.style.right === undefined && this.props.style.top === undefined && this.props.style.bottom === undefined)) {
-                sty.left = sty.right = sty.top = sty.bottom = 0;
-            }
-            // style.position MUST is 'absolute'
-            sty.position = 'absolute';
-            sty.zIndex = 999999999;
 
-            return <View key={this.$Name} style={sty}>{this.props.children}</View>;
+            // disable BackPress
+            this.disableBackPress = () => {
+                if(!_hardwareBackPressHandler) {
+                    _hardwareBackPressHandler = BackHandler.addEventListener("hardwareBackPress", () => {
+                        return true;
+                    });
+                }
+            };
+
+            // remove disable BackPress
+            this.removeDisableBackPress = () => {
+                if(_hardwareBackPressHandler) {
+                    _hardwareBackPressHandler.remove();
+                    _hardwareBackPressHandler = undefined;
+                }
+            };
+        }
+
+        get scopeState() {
+            if(this._overlayItem) {
+                return this._overlayItem.state;
+            }
+            return {};
+        }
+
+        setScopeState(a, b) {
+            if(this._overlayItem) {
+                this._overlayItem.setState(a, b);
+            }
+            return this;
         }
       
         componentDidMount() {
@@ -98,7 +122,7 @@ if(!ReactNative.Overlay) {
         }
       
         componentDidUpdate() {
-            this.visible = !!this.props.visible;
+            this.visible = this.visible;
         }
 
         close() {
@@ -108,12 +132,52 @@ if(!ReactNative.Overlay) {
         show() {
             this.visible = true;
         }
+
+        apply() {
+            this.visible = this.visible;
+        }
       
       
         render() {
             return <></>;
         }
       
+    }
+
+
+    class OverlayItem extends React.Component {
+        constructor(props) {
+            super(props);
+
+            if(props.overlay) {
+                props.overlay._overlayItem = this;
+            }
+
+            this.state = props.scopeState ? {...props.scopeState} : {};
+        }
+
+        render() {
+            let sty = {backgroundColor: 'rgba(88, 88, 88, 0.6)', ...(this.props.style || {})};
+            // if not defined STYLE, or defined POSITON(left|right|top|bottom), or defined size(width|height), then use default value({left:0, right:0, top:0, bottom:0})
+            if(!this.props.style) {
+                sty.left = sty.right = sty.top = sty.bottom = 0;
+            } else {
+                if(this.props.style.width === undefined && this.props.style.left === undefined && this.props.style.right === undefined) {
+                    sty.left = sty.right = 0;
+                }
+                if(this.props.style.height === undefined && this.props.style.top === undefined && this.props.style.bottom === undefined) {
+                    sty.top = sty.bottom = 0;
+                }
+            }
+            // style.position MUST is 'absolute'
+            sty.position = 'absolute';
+            sty.zIndex = 999999999;
+
+            return <View key={this.$Name} style={sty}>
+                {this.props.children instanceof Function ? this.props.children.apply(this.props.overlay, []) : this.props.children}
+            </View>;
+        }
+
     }
 
 
@@ -126,8 +190,8 @@ if(!ReactNative.Overlay) {
             this.state = {
                 childKeys: [] // [ OverlayName.... ]
             };
-      
-            this.childs = {}; // Overlay instances
+
+            this.childs = {};
         }
       
         addOrUpdateChild(overlay) {
@@ -136,8 +200,9 @@ if(!ReactNative.Overlay) {
                 if(overlay.visible) {
                     if(idx < 0) {
                         state.childKeys.push(overlay.$Name);
+                        this.childs[overlay.$Name] = overlay;
                     }
-                    this.childs[overlay.$Name] = overlay;
+                    
                 } else {
                     if(idx >= 0) {
                         state.childKeys.splice(idx, 1);
@@ -148,11 +213,36 @@ if(!ReactNative.Overlay) {
                 return state;
             });
         }
+
+        clear() {
+            let ks = this.state.childKeys.map((K) => K);
+            // this.setState((state) => {
+            //     state.childKeys.splice(0, state.childKeys.length);
+            // });
+            ks.forEach((K) => {
+                if(this.childs[K]) {
+                    this.childs[K].close();
+                }
+            });
+        }
       
         render() {
             return <>
                 {
-                    this.state.childKeys.map(K => this.childs[K].$View)
+                    this.state.childKeys.map(K => {
+                        let overlay = this.childs[K];
+                        if(overlay) {
+                            return <OverlayItem
+                                key={overlay.$Name}
+                                name={overlay.$Name}
+                                scopeState={overlay.props.scopeState}
+                                style={overlay.props.style}
+                                children={overlay.props.children}
+                                overlay={overlay}
+                                ></OverlayItem>;
+                        }
+                        return null;
+                    })
                 }
             </>;
         }
@@ -214,6 +304,16 @@ if(!ReactNative.Overlay) {
         let idx = Modals.findIndex((T) => T == modal);
         if(idx >= 0) {
             Modals.splice(idx, 1);
+            let overlayContainer = modal.overlayContainer || modal._overlayContainer;
+            // remove all hardwareBackPress handler
+            if(overlayContainer) {
+                let ks = Object.keys(overlayContainer.childs);
+                ks.forEach((K) => {
+                    overlayContainer.childs[K].removeDisableBackPress();
+                });
+
+                // overlayContainer.clear();
+            }
         }
     };
 
@@ -221,7 +321,7 @@ if(!ReactNative.Overlay) {
     // add Function addOrUpdateOverlay to Modal
     // Add Or Update a Overlay
     Modal.prototype.addOrUpdateOverlay = function(overlay) {
-        this.overlayContainer.addOrUpdateChild(overlay);
+        (this.overlayContainer || this._overlayContainer).addOrUpdateChild(overlay);
     };
 
 
@@ -229,6 +329,12 @@ if(!ReactNative.Overlay) {
     // issue: https://github.com/facebook/react-native/issues/29319
     let _modalComponentDidUpdate = Modal.prototype.componentDidMount;
     Modal.prototype.componentDidUpdate = function(prevProps) {
+        if (this.props.visible !== true) {
+            delModal(this);
+        } else {
+            newModal(this);
+        }
+        this._overlayContainer = this.overlayContainer;
         if(!!!this.props.visible && !!prevProps.visible && this.props.onDismiss) {
             if(this.props.onDismiss) {
                 this.props.onDismiss.apply(this, []);
@@ -242,10 +348,10 @@ if(!ReactNative.Overlay) {
     // re-write Modal's render()
     Modal.prototype.render = function() {
         if (this.props.visible !== true) {
-            delModal(this);
+            // delModal(this);
             return null;
         } else {
-            newModal(this);
+            // newModal(this);
         }
       
         const containerStyles = {
